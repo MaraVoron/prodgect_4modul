7c9cefd13e26b99e4b29dd4e43b2106a880a8845
Воронцова Мара <mari.gorina2006@mail.ru>
2023-08-10 18:01:46 +0300

    inital

---
+++ lesson_1.py
@@ -0,0 +1,21 @@
+"""
+def strcount(s):  #N**2
+    for sym in set(s):
+        counter = 0
+        for sab_sym in s:
+            if sym == sab_sym:
+                counter += 1
+        print(sym, counter)
+
+strcount('aabdssra')
+"""
+
+def strcount(s):
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+
+    for sym, count in syms_counter.items():
+        print(sym, count)
+
+strcount('adfjfnrfjnrnurrbha')
